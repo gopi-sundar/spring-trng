0bd51eaa937e7e98d95ff19e9f4c9d5f091b9a6c
Sundararaju, Gopinath <gopinath.sundararaju@pearson.com>
2017-11-29 16:13:35 -0600

    b12 mix xml and annotations

--- src/main/java/com/arizon/spring/basics/springtrng/SpringTrngXmlContextApplication.java
+++ src/main/java/com/arizon/spring/basics/springtrng/SpringTrngXmlContextApplication.java
@@ -1,18 +1,23 @@
 package com.arizon.spring.basics.springtrng;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.arizon.spring.basics.springtrng.xml.XmlPersonDAO;
 
 public class SpringTrngXmlContextApplication {
 
+	private static Logger LOGGER = LoggerFactory.getLogger("SpringTrngXmlContextApplication.class");
+
 	public static void main(String[] args) {
 
-		// Application Context
-		// ApplicationContext applicationContext =
-		// SpringApplication.run(SpringTrngBasicApplication.class, args);
 		try (ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(
 				"applicationContext.xml")) {
+
+			LOGGER.info("Beans loaded are > {}", (Object) applicationContext.getBeanDefinitionNames());
+			// Beans loaded are > [xmlJdbcConnection, xmlPearsonDAO]
+
 			XmlPersonDAO xmlPersonDAO = applicationContext.getBean(XmlPersonDAO.class);
 			System.out.println(xmlPersonDAO);
 
